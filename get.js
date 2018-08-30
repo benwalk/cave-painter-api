809e134602cd9297148252aa048b971660f3d478
Ben Walker <benwalker@benwalker.local>
2018-08-29 21:43:09 -0700

    deployed infra

--- get.js
+++ get.js
@@ -3,18 +3,18 @@ import { success, failure } from "./libs/responses";
 
 export async function main(event, context, callback) {
   const params = {
-    TableName: "notes",
+    TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
+      reference: event.pathParameters.id
     }
   };
 
   try {
-    const result = await dynamoDbLib.call("get", params);
+    const result = await postgres.call("get", params);
     if (result.Item) {
       // Return the retrieved item
       callback(null, success(result.Item));
